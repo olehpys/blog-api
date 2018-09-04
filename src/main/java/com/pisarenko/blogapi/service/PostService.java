fdf56d4642f8107bc3268c2dd1f7a6f14121ae58
pisarenko <oa.pisarenko@gmail.com>
2018-09-04 19:48:35 +0300

    services for posts

---
+++ src/main/java/com/pisarenko/blogapi/service/PostService.java
@@ -0,0 +1,21 @@
+package com.pisarenko.blogapi.service;
+
+import com.pisarenko.blogapi.model.Post;
+
+import java.util.List;
+
+public interface PostService {
+
+    Post createPost(Post post);
+
+    void editPost(Post post);
+
+    void deletePost(long id);
+
+    List<Post> getPostById(long id);
+
+    List<Post> getAllPosts();
+
+    List<Post> getAllAuthorPosts(long id);
+
+}
---
+++ src/main/java/com/pisarenko/blogapi/service/impl/PostServiceImpl.java
@@ -0,0 +1,50 @@
+package com.pisarenko.blogapi.service.impl;
+
+import com.pisarenko.blogapi.model.Post;
+import com.pisarenko.blogapi.repository.PostRepository;
+import com.pisarenko.blogapi.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void editPost(Post post) {
+        postRepository.save(post);
+    }
+
+    @Override
+    public void deletePost(long id) {
+        postRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Post> getPostById(long id) {
+        return postRepository.findPostById(id);
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public List<Post> getAllAuthorPosts(long id) {
+        return postRepository.findPostsByAuthorId(id);
+    }
+}
