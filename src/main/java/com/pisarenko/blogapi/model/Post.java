47ca9e8efb16c22bfaae393cbada59d9f8f00403
pisarenko <oa.pisarenko@gmail.com>
2018-09-04 19:09:53 +0300

    created object models

---
+++ src/main/java/com/pisarenko/blogapi/model/Post.java
@@ -0,0 +1,54 @@
+package com.pisarenko.blogapi.model;
+
+public class Post {
+
+    private long id;
+
+    private String title;
+
+    private String content;
+
+    private User author;
+
+    public Post() {
+    }
+
+    public Post(long id, String title, String content, User author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/pisarenko/blogapi/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.pisarenko.blogapi.model;
+
+public enum UserRole {
+    PUBLISHER
+}
