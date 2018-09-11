1cd77b7c15c815c2b59191a09abcc7ca9ac7f331
pisarenko <oa.pisarenko@gmail.com>
2018-09-11 14:11:36 +0300

    user password encryption

--- src/main/java/com/pisarenko/blogapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/pisarenko/blogapi/service/impl/UserServiceImpl.java
@@ -4,20 +4,24 @@ import com.pisarenko.blogapi.model.User;
 import com.pisarenko.blogapi.repository.UserRepository;
 import com.pisarenko.blogapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+        passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
     public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
