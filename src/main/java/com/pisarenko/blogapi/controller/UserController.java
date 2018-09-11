cbbdcf3a313b6969db8ba07068eb0cc79946ec0b
pisarenko <oa.pisarenko@gmail.com>
2018-09-11 16:44:48 +0300

    refactoring

--- src/main/java/com/pisarenko/blogapi/controller/UserController.java
+++ src/main/java/com/pisarenko/blogapi/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserController {
 
-
     private UserService userService;
 
     @Autowired
