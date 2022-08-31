b2153cf9aeb77cbf58a87328105365dc3fde02a4
Oleh Pysarenko <oa.pisarenko@gmail.com>
2022-08-31 10:17:30 +0100

    added @Builder

--- src/main/java/com/pysarenko/blog/entity/Comment.java
+++ src/main/java/com/pysarenko/blog/entity/Comment.java
@@ -9,11 +9,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
--- src/main/java/com/pysarenko/blog/entity/Post.java
+++ src/main/java/com/pysarenko/blog/entity/Post.java
@@ -10,11 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
